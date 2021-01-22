b36a8dc1e5abbf66b7e732bc318fe6b5a5117a9a
derekhingeveld <derekhingeveld@gmail.com>
2021-01-22 09:48:38 -0700

    Finished section 3

--- 03_Components_State_and_Props/github-finder/src/components/users/UserItem.js
+++ 03_Components_State_and_Props/github-finder/src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
+const UserItem = ({user: { login, avatar_url, html_url }}) => {
     return (
       <div className="card text-center">
         <img src={avatar_url} alt="" className='round-img' style={{ width: '60px' }}/>
@@ -13,8 +11,11 @@ class UserItem extends Component {
           <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
       </div>
-    )
-  }
-}
+    );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem
