5453c67e79f68b4169caa0632a550e6e4e5488b1
Derek Hingeveld <derekhingeveld@gmail.com>
2021-01-27 15:34:38 -0700

    Finished section 5

--- 05_Refactoring_to_Hooks_and_Context/github-finder/src/context/github/githubReducer.js
+++ 05_Refactoring_to_Hooks_and_Context/github-finder/src/context/github/githubReducer.js
@@ -3,41 +3,41 @@ import {
   GET_USER,
   CLEAR_USERS,
   GET_REPOS,
-  SET_LOADING
+  SET_LOADING,
 } from '../types';
 
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SEARCH_USERS:
       return {
         ...state,
         users: action.payload,
-        loading: false
+        loading: false,
       };
     case GET_USER:
       return {
         ...state,
         user: action.payload,
-        loading: false
-      }
+        loading: false,
+      };
     case CLEAR_USERS:
       return {
         ...state,
         users: [],
-        loading: false
+        loading: false,
       };
     case GET_REPOS:
       return {
         ...state,
         repos: action.payload,
-        loading: false
+        loading: false,
       };
     case SET_LOADING:
       return {
         ...state,
-        loading: true
+        loading: true,
       };
-      default:
-        return state;
+    default:
+      return state;
   }
-}
+};
